c96ae794214f9d1d47110e796026185c930bc6d3
fsouzadi1995 <fsouzadi1995@gmail.com>
2020-10-22 01:00:56 -0300

    Updated config file

--- infrastructure/url_builder.js
+++ infrastructure/url_builder.js
@@ -5,7 +5,7 @@ const config = configLoader.GetDBConfig();
 const buildUrl = (db) => {
   return config.db_data.baseUri
     .replace('<user>', config.db_data.cred.name)
-    .replace('<password>', config.db_data.cred.pw)
+    .replace('<password>', config.db_data.cred.password)
     .replace('<db>', db);
 };
 
