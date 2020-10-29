393a94da335b12f281fe868f138bf3095216e970
fsouzadi1995 <fsouzadi1995@gmail.com>
2020-10-29 00:18:32 -0300

    🎨 Reflecting change on url-builder

--- infrastructure/url-builder.js
+++ infrastructure/url-builder.js
@@ -4,8 +4,8 @@ const config = configLoader.GetDBConfig();
 
 const build = () => {
   return config.db_data.baseUri
-    .replace('<user>', config.db_data.cred.name)
-    .replace('<password>', config.db_data.cred.password)
+    .replace('<user>', config.db_data.creds.name)
+    .replace('<password>', config.db_data.creds.password)
     .replace('<db>', '');
 };
 
